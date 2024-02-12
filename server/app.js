f881649e48a7239cdd0ed854f004b88eb4627b7f
cammyvonwammy <cameron.brett32@gmail.com>
2024-02-12 11:50:44 +1000

    initial commit

---
+++ server/app.js
@@ -0,0 +1,32 @@
+// server/app.js
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+// Import routes
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+
+// Use routes
+app.use('/api/users', userRoutes);
+
+// Root route for testing
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://cameronbrett32:V8Fcn2pdrvkXlrBK@dev-myapp.p7zdjqk.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Only start the server if MongoDB connects successfully
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch(err => console.error('Could not connect to MongoDB:', err));
+
